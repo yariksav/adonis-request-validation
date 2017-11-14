b345d3cc5854844704e4655e9b4fc0327cc37ed3
Harminder Virk <virk.officials@gmail.com>
2017-11-14 12:19:54 +0530

    feat(*): add validateAll macro

--- providers/RequestValidationProvider.js
+++ providers/RequestValidationProvider.js
@@ -12,15 +12,22 @@ class RequestValidationProvider extends ServiceProvider {
 
   boot () {
     const Request = this.app.use('Adonis/Src/Request')
-    const { validateAll } = this.app.use('@adonisjs/validator/src/Validator')
+    const { validateAll, validate } = this.app.use('@adonisjs/validator/src/Validator')
     const CE = this.app.use('@adonisjs/validator/src/Exceptions')
 
     /**
      * Extend request class by adding a macro, which pushes a
      * possibility to validate params directly
      */
-    Request.macro('validate', async function (rules) {
-      let validation = await validateAll(this.all(), rules)
+    Request.macro('validate', async function (rules, messages = {}) {
+      let validation = await validate(this.all(), rules, messages)
+      if (validation.fails()) {
+        throw CE.ValidationException.validationFailed(validation.messages())
+      }
+    })
+
+    Request.macro('validateAll', async function (rules, messages = {}) {
+      let validation = await validateAll(this.all(), rules, messages)
       if (validation.fails()) {
         throw CE.ValidationException.validationFailed(validation.messages())
       }
